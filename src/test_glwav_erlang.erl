e9499f9ae3c18d07d70d3188bbdd3126a7e5b286
haruki7049 <tontonkirikiri@gmail.com>
2026-01-02 08:48:13 +0900

    Update

--- src/test_glwav_erlang.erl
+++ src/test_glwav_erlang.erl
@@ -1,6 +1,9 @@
 -module(test_glwav_erlang).
 -export([main/1]).
 
+
+-spec main(_Args) -> any()
+              when _Args :: [string()].
 main(_Args) ->
     Wave = {wave, p_c_m, 44100, 1, i16, []},
     WaveBits = glwav:to_bit_array(Wave),
